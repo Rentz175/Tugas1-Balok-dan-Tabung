b083feb94d614d8b0ce5a8c562b2ad8fa8c6a6ab
Rentz175 <febrianmumta89@gmail.com>
2023-03-01 18:10:42 +0700

    0.2

--- src/tugas1/Tugas1.java
+++ src/tugas1/Tugas1.java
@@ -4,6 +4,8 @@
  */
 package tugas1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Febrian
@@ -13,24 +15,79 @@ public class Tugas1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-//        PersegiPanjang pp = new PersegiPanjang(6,12);
-//       
-//        System.out.println("Luas Persegi Panjang = " + pp.hitungLuas());
-//        System.out.println("Keliling Persegi Panjang = " + pp.hitungKeliling());
-//        
-//        Lingkaran o = new Lingkaran(10);
-//        System.out.println("Luas Lingkaran = " + o.hitungLuas());
-//        System.out.println("Keliling Lingkaran = " + o.hitungKeliling());
+    public static void main(String[] args){
         
-        Balok bl = new Balok(6, 6, 12 );
-        System.out.println("Luas Persegi Panjang = " + bl.hitungLuas());
-        System.out.println("Volume Balok = " + bl.hitungVolumeBalok());
-        System.out.println("Luas Permukaan Balok = " + bl.hitungLuasBalok());
+        Scanner input = new Scanner(System.in);
         
-        Tabung tb = new Tabung(10,10);
-        System.out.println("Volume Tabung = " + tb.hitungVolumeTabung());
-        System.out.println("Luas Permukaan Tabung = " + tb.hitungLuasTabung());
+        int menu;
+        char ulang;
+        int panjang, lebar, tinggi, jarijari;
+        
+        do {
+            System.out.println("==========");
+            System.out.println("MENU UTAMA");
+            System.out.println("==========");
+            System.out.println("1.Hitung Balok");
+            System.out.println("2.Hitung Tabung");
+            System.out.println("0.Exit");
+            System.out.print("Pilih : ");
+            menu = input.nextInt();
+            
+            System.out.println("============");
+            
+            switch (menu) {
+                case 1:
+                    System.out.println("Hitung Balok");
+                    System.out.println("------------");
+                    System.out.print("Input Panjang : ");
+                    panjang = input.nextInt();
+                    System.out.print("Input Lebar   : ");
+                    lebar = input.nextInt();
+                    System.out.print("Input Tinggi  : ");
+                    tinggi = input.nextInt();
+                    
+                    Balok bl = new Balok(tinggi, panjang, lebar);
+                    
+                    System.out.println("========");
+                    System.out.println("Hasil : ");
+                    System.out.println("--------");
+                    System.out.println("Luas Persegi Panjang    = " + bl.hitungLuas());
+                    System.out.println("Keliling Persegi Panjang= " + bl.hitungKeliling());
+                    System.out.println("Volume Balok            = " + bl.hitungVolumeBalok());
+                    System.out.println("Luas Permukaan Balok    = " + bl.hitungLuasBalok());
+                    break;
+                    
+                case 2: 
+                    System.out.println("Hitung Tabung");
+                    System.out.println("-------------");
+                    System.out.print("Input Jari-Jari : ");
+                    jarijari = input.nextInt();
+                    System.out.print("Input Tinggi    : ");
+                    tinggi = input.nextInt();
+                    
+                    Tabung tb = new Tabung(tinggi,jarijari);
+                    
+                    System.out.println("========");
+                    System.out.println("Hasil : ");
+                    System.out.println("--------");
+                    System.out.println("Luas Lingkaran        = " + tb.hitungLuas());
+                    System.out.println("Keliling Lingkaran    = " + tb.hitungKeliling());
+                    System.out.println("Volume Tabung         = " + tb.hitungVolumeTabung());
+                    System.out.println("Luas Permukaan Tabung = " + tb.hitungLuasTabung());
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                    
+                default:
+                    System.out.println("Menu Tidak Ada");
+            }
+            System.out.println("===============");
+            System.out.print("Kembali? (y/n) : ");
+            ulang = input.next().charAt(0);
+            
+        } while (ulang != 'n');
+       
     }
     
 }
